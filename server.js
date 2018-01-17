c6b86acce81feb97863c604d8ea2e9e8d89dc477
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-17 10:13:21 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const server = express();
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
