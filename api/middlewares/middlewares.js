2929f9a1c62310a46101c19c0a280e6e5f5b2812
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-18 08:21:40 -0500

    Added middlewares for authenication checking

--- api/middlewares/middlewares.js
+++ api/middlewares/middlewares.js
@@ -1,2 +1,66 @@
-/ Static files
-server.us(express.static())
+const User = require('../models/Users/UserSchema');
+const bcrypt = require('bcrypt');
+
+const STATUS_USER_ERROR = 422;
+const BCRYPT_COST = 11;
+
+const sendUserError = (err, res) => {
+  res.status(STATUS_USER_ERROR);
+  if (err && err.message) {
+    res.json({ message: err.message, stack: err.stack });
+  } else {
+    res.json({ error: err });
+  }
+};
+
+/* ************Middelwares *************** */
+const hashedPassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    sendUserError('Must have a password', res);
+  }
+  bcrypt
+    .hash(password, BCRYPT_COST)
+    .then((pw) => {
+      req.password = pw;
+      next();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
+
+const loggedIn = (req, res, next) => {
+  const { username } = req.session;
+  if (!username) {
+    sendUserError('User is not logged in', res);
+  }
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      sendUserError(err, res);
+    } else if (!user) {
+      sendUserError('User doees not exist', res);
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
+
+const restrictedPermissions = (req, res, next) => {
+  const { path } = req;
+  if (/restricted/.test(path)) {
+    if (!req.session.username) {
+      sendUserError('User is not authorized', res);
+    }
+  }
+  next();
+};
+
+module.exports = {
+  sendUserError,
+  hashedPassword,
+  loggedIn,
+  restrictedPermissions,
+};
+
